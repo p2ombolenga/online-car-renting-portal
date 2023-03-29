6b439e7085db60ec0ff469e655e29cd717eac631
Nsabimana peter <nsabimanapeter2000@gmail.com>
2023-03-29 17:31:15 +0200

    uploading to cloud

---
+++ about-us.php
@@ -0,0 +1,186 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if(isset($_POST['send']))
+  {
+$name=$_POST['fullname'];
+$email=$_POST['email'];
+$contactno=$_POST['contactno'];
+$message=$_POST['message'];
+$sql="INSERT INTO  tblcontactusquery(name,EmailId,ContactNumber,Message) VALUES(:name,:email,:contactno,:message)";
+$query = $dbh->prepare($sql);
+$query->bindParam(':name',$name,PDO::PARAM_STR);
+$query->bindParam(':email',$email,PDO::PARAM_STR);
+$query->bindParam(':contactno',$contactno,PDO::PARAM_STR);
+$query->bindParam(':message',$message,PDO::PARAM_STR);
+$query->execute();
+$lastInsertId = $dbh->lastInsertId();
+if($lastInsertId)
+{
+$msg="Query Sent. We will contact you shortly";
+}
+else 
+{
+$error="Something went wrong. Please try again";
+}
+
+}
+?>
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<meta name="keywords" content="">
+<meta name="description" content="">
+<title>CarForYou - Responsive Car Dealer HTML5 Template</title>
+<!--Bootstrap -->
+<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
+<!--Custome Style -->
+<link rel="stylesheet" href="assets/css/style.css" type="text/css">
+<!--OWL Carousel slider-->
+<link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
+<link rel="stylesheet" href="assets/css/owl.transitions.css" type="text/css">
+<!--slick-slider -->
+<link href="assets/css/slick.css" rel="stylesheet">
+<!--bootstrap-slider -->
+<link href="assets/css/bootstrap-slider.min.css" rel="stylesheet">
+<!--FontAwesome Font Style -->
+<link href="assets/css/font-awesome.min.css" rel="stylesheet">
+
+<!-- SWITCHER -->
+		<link rel="stylesheet" id="switcher-css" type="text/css" href="assets/switcher/css/switcher.css" media="all" />
+		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/red.css" title="red" media="all" data-default-color="true" />
+		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/orange.css" title="orange" media="all" />
+		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/blue.css" title="blue" media="all" />
+		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/pink.css" title="pink" media="all" />
+		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/green.css" title="green" media="all" />
+		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/purple.css" title="purple" media="all" />
+        
+<!-- Fav and touch icons -->
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/images/favicon-icon/apple-touch-icon-144-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/images/favicon-icon/apple-touch-icon-114-precomposed.html">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/images/favicon-icon/apple-touch-icon-72-precomposed.png">
+<link rel="apple-touch-icon-precomposed" href="assets/images/favicon-icon/apple-touch-icon-57-precomposed.png">
+<link rel="shortcut icon" href="assets/images/favicon-icon/favicon.png">
+<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+ <style>
+    .errorWrap {
+    padding: 10px;
+    margin: 0 0 20px 0;
+    background: #fff;
+    border-left: 4px solid #dd3d36;
+    -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+    box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+}
+.succWrap{
+    padding: 10px;
+    margin: 0 0 20px 0;
+    background: #fff;
+    border-left: 4px solid #5cb85c;
+    -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+    box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+}
+    </style>
+</head>
+<body>
+
+        
+<!--Header-->
+<?php include('includes/header.php');?>
+<!-- /Header --> 
+
+<!--Page Header-->
+<section class="page-header aboutus_page mb-5">
+  <div class="container">
+    <div class="page-header_wrap">
+      <div class="page-heading">
+        <h1>About Us</h1>
+      </div>
+      <ul class="coustom-breadcrumb">
+        <li><a href="#">Home</a></li>
+        <li>About Us</li>
+      </ul>
+    </div>
+  </div>
+  <!-- Dark Overlay-->
+  <div class="dark-overlay"></div>
+</section>
+<!-- /Page Header--> 
+
+<div class="container mt-5" style="margin-top: 5rem;">
+  <div class="col-md-12">
+    <h1 class="mb-4">Welcome to our Online Car Rental Portal</h1>
+    <p>We strive to make your car rental experience easy, convenient, and affordable. Our website is designed to offer you a user-friendly platform where you can book your car rental in just a few clicks.</p>
+
+    <div class="row mt-4">
+      <div class="col-md-6">
+        <h2>Choose Your Car</h2>
+        <p>We offer a wide range of cars to choose from, including economy cars, luxury cars, SUVs, and more. We partner with trusted car rental agencies to ensure that you have access to high-quality, well-maintained vehicles.</p>
+      </div>
+      <div class="col-md-6">
+        <h2>Book Your Rental</h2>
+        <p>Our website is designed to make the car rental process as seamless as possible. We offer a simple booking process that allows you to select the type of car you want, choose the pickup and drop-off locations, and select the dates of your rental. You can also add any additional services you may need, such as a GPS device or child car seat.</p>
+      </div>
+    </div>
+
+    <div class="row mt-4">
+      <div class="col-md-6">
+        <h2>Flexible Rental Periods</h2>
+        <p>We understand that flexibility is important when it comes to car rentals, which is why we offer a range of rental periods to suit your needs. Whether you need a car for a day, a week, or longer, we have options to meet your requirements.</p>
+      </div>
+      <div class="col-md-6">
+        <h2>Excellent Customer Service</h2>
+        <p>We take pride in our commitment to excellent customer service, and our team is always available to answer any questions you may have. If you encounter any issues during the booking process or with your rental, we are here to help.</p>
+      </div>
+    </div>
+
+    <div class="row mt-4">
+      <div class="col-md-6">
+<h2>Affordable Prices</h2>
+<p>We believe that car rental should be accessible to everyone, which is why we offer competitive pricing and frequent discounts and promotions. We also offer a loyalty program that rewards our regular customers with discounts and other perks.</p>
+</div>
+</div>
+</div>
+</div>
+
+
+<!--Footer -->
+<?php include('includes/footer.php');?>
+<!-- /Footer--> 
+
+<!--Back to top-->
+<div id="back-top" class="back-top"> <a href="#top"><i class="fa fa-angle-up" aria-hidden="true"></i> </a> </div>
+<!--/Back to top--> 
+
+<!--Login-Form -->
+<?php include('includes/login.php');?>
+<!--/Login-Form --> 
+
+<!--Register-Form -->
+<?php include('includes/registration.php');?>
+
+<!--/Register-Form --> 
+
+<!--Forgot-password-Form -->
+<?php include('includes/forgotpassword.php');?>
+<!--/Forgot-password-Form --> 
+
+<!-- Scripts --> 
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script> 
+<script src="assets/js/interface.js"></script> 
+<!--Switcher-->
+<script src="assets/switcher/js/switcher.js"></script>
+<!--bootstrap-slider-JS--> 
+<script src="assets/js/bootstrap-slider.min.js"></script> 
+<!--Slider-JS--> 
+<script src="assets/js/slick.min.js"></script> 
+<script src="assets/js/owl.carousel.min.js"></script>
+
+</body>
+
+<!-- Mirrored from themes.webmasterdriver.net/carforyou/demo/contact-us.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 16 Jun 2017 07:26:55 GMT -->
+</html>
